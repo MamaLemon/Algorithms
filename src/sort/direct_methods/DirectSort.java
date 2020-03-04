9d1dbe5e8a8109033f45447d809bc8013807a14b
MamaLemon <hohlov-2000@mail.ru>
2020-03-04 21:12:04 +0300

    Created functions

---
+++ src/sort/direct_methods/DirectSort.java
@@ -0,0 +1,18 @@
+package sort.direct_methods;
+
+import java.util.List;
+
+public class DirectSort {
+    public List <Integer> directInclusion(List <Integer> list){
+
+        return list;
+    }
+    public List <Integer> directSelection(List <Integer> list){
+
+        return list;
+    }
+    public List <Integer> directExchange(List <Integer> list){
+
+        return list;
+    }
+}
