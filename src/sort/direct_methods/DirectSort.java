9f1a001e4d9316d1c60f3201f98619ff34826ddb
MamaLemon <hohlov-2000@mail.ru>
2020-03-06 01:52:19 +0300

    Add direct inclusion and direct selection

--- src/sort/direct_methods/DirectSort.java
+++ src/sort/direct_methods/DirectSort.java
@@ -1,18 +1,66 @@
 package sort.direct_methods;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DirectSort {
-    public List <Integer> directInclusion(List <Integer> list){
+    public static int[] directInclusion(int[] list){
+        int j = 0;
+        int elementInclusion;
+        for(int i = 1; i < list.length; i++){
+            elementInclusion = list[i];
+            j = i - 1;          //индекс предыдущего элемента
+            //сравниваем эл-ты от данного до нулевого
+            while(j >= 0 && elementInclusion < list[j]) {
+                list[j + 1] = list[j];
+                j--;
+            }
+            list[j+1] = elementInclusion;
+        }
+        return list;
+    }
+    public static int[] directSelection(int[] list){
+        int elementSelection;
+        int indexElementSelection;
+        for(int i = 0; i < list.length-1; i++){
+            elementSelection = list[i];
+            indexElementSelection = i;
+            for(int j = i + 1; j < list.length; j++){
+                if(list[j] < elementSelection){
+                    indexElementSelection = j;
+                    elementSelection = list[indexElementSelection];
+                }
+            }
+            //меняем местами элементы
+            list[indexElementSelection] = list[i];
+            list[i] = elementSelection;
+        }
+        return list;
+    }
+    public static int[] directExchange(int[] list){
 
         return list;
     }
-    public List <Integer> directSelection(List <Integer> list){
 
-        return list;
-    }
-    public List <Integer> directExchange(List <Integer> list){
-
-        return list;
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++){
+            try {
+                array[i] = Integer.parseInt(reader.readLine());
+            }
+            catch (IOException e){
+                System.out.println("I have a problem!");
+            }
+        }
+        for (int x: array) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        array =  directSelection(array);
+        for (int x: array) {
+            System.out.print(x + " ");
+        }
     }
 }
